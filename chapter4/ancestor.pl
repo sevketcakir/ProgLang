7ce667e17ad82df61ee49fca2f3fd1ab8a93c7cb
sevketcakir <cakiaccounts@gmail.com>
2016-04-11 15:26:22 +0300

    Prolog ancestor, append, reverse ve cons örnekleri 11.04.2016

---
+++ chapter4/ancestor.pl
@@ -0,0 +1,20 @@
+%Kitapta bulunan ancestor
+ancestor(X,Y):-parent(X,Z),ancestor(Z,Y).
+ancestor(X,X).
+parent(amy,bob).
+% Derste yazılan ancestor1
+ancestor1(X,Y):-parent1(X,Y).
+ancestor1(X,Y):-parent1(X,Z),ancestor1(Z,Y).
+parent1(amy,bob).
+parent1(bob,john).
+
+gcd(U,0,U).
+gcd(U,V,W):-not(V=0),R is U mod V,gcd(V,R,W).
+
+cons(X,Y,[X|Y]).
+
+append([],X,X).
+append([A|B],Y,[A|W]):-append(B,Y,W).
+
+reverse([],[]).
+reverse([H|T],L):-reverse(T,L1),append(L1,[H],L).
